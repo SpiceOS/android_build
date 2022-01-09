maffeen/build

core: Only include Fluid SE policy if repo exists
Change-Id: If799106d2da6d8c303430e316edff365a22f7c99

core/config.mk
@@ -1245,9 +1245,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
 ifneq ($(FLUID_BUILD),)
+ifneq ($(wildcard device/fluid/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/fluid/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 # Include any vendor specific config.mk file

SpiceOS/android_build

Signed-off-by: Anush02198 <Anush.4376@gmail.com>

core/config.mk
@@ -1176,9 +1176,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))
 
 ifneq ($(SPICEOS_BUILD),)
+ifneq ($(wildcard device/spiceos/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/spiceos/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 # Include any vendor specific config.mk file