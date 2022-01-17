maffeen/build

Remove LatinIME as it is included per target in vendor/fluid
Change-Id: Ibce4a244ec0653a7c7798d864047ac667866a1dc

target/product/handheld_product.mk
@@ -28,13 +28,17 @@ PRODUCT_PACKAGES += \
     Contacts \
     DeskClock \
     Gallery2 \
-    LatinIME \
     Music \
     OneTimeInitializer \
     preinstalled-packages-platform-handheld-product.xml \
     QuickSearchBox \
     SettingsIntelligence \
     frameworks-base-overlays
+
+ifeq ($(FLUID_BUILD),)
+PRODUCT_PACKAGES += \
+    LatinIME
+endif
 
 PRODUCT_PACKAGES_DEBUG += \
     frameworks-base-overlays-debug

SpiceOS/android_build

Signed-off-by: Anush02198 <Anush.4376@gmail.com>

target/product/handheld_product.mk
@@ -28,13 +28,17 @@ PRODUCT_PACKAGES += \
     Contacts \
     DeskClock \
     Gallery2 \
-    LatinIME \
     Music \
     OneTimeInitializer \
     preinstalled-packages-platform-handheld-product.xml \
     QuickSearchBox \
     SettingsIntelligence \
     frameworks-base-overlays
+
+ifeq ($(SPICEOS_BUILD),)
+PRODUCT_PACKAGES += \
+    LatinIME
+endif
 
 PRODUCT_PACKAGES_DEBUG += \
     frameworks-base-overlays-debug